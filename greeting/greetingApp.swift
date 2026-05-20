a7250fd81d954bf3906f0dccbfafdbef821935e6
DigitalTurkk <huseynovfarid1111@gmail.com>
2026-05-20 23:41:17 +0400

    Initial Commit

---
+++ greeting/greetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  greetingApp.swift
+//  greeting
+//
+//  Created by Farid on 20.05.26.
+//
+
+import SwiftUI
+
+@main
+struct greetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
